02572197c35bf5d35418870a6d7b16fa12eff3ae
Yar <yarsher.dev@gmail.com>
2019-02-01 13:00:10 -0500

    permission + intentFilters

--- app/src/main/java/com/yarsher/at/droid2droid/MainActivity.java
+++ app/src/main/java/com/yarsher/at/droid2droid/MainActivity.java
@@ -1,13 +1,37 @@
 package com.yarsher.at.droid2droid;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.nio.channels.Channel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+
+
     }
 }
